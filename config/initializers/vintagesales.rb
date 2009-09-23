e3a44b84596ad5df3c59c2117ee4c1f5370c2020
Peter pete <peterhinshaw@gmail.com>
2009-09-22 21:44:25 -0500

    tysvintagesales customizations

---
+++ config/initializers/vintagesales.rb
@@ -0,0 +1,8 @@
+
+require 'vendor/extensions/vintagesales/app/models/calculator/vintagesales_standard_calculator'
+require 'vendor/extensions/vintagesales/app/models/calculator/vintagesales_priority_calculator'
+require 'vendor/extensions/vintagesales/app/models/calculator/vintagesales_overnight_calculator'
+
+Calculator::VintageSalesOvernightCalculator
+Calculator::VintageSalesPriorityCalculator
+Calculator::VintageSalesStandardCalculator
---
+++ vendor/extensions/vintagesales/app/models/calculator/vintagesales_standard_calculator.rb
@@ -0,0 +1,27 @@
+class Calculator::VintageSalesStandardCalculator < Calculator
+
+  def self.description
+    I18n.t("vs_standard_shipping")
+  end
+
+  def self.register
+    super                                
+    ShippingMethod.register_calculator(self)
+    ShippingRate.register_calculator(self)
+  end
+  
+  def compute(order)
+    return if order.nil?
+    return (3.0 * order.size) if order[0].variant.product.taxons[0].name == "Books"
+    
+    @index = 0
+    order[0].variant.product.properties.each {|x|
+       if x.name == "standard shipping" 
+	   @property_value = order[0].variant.product.product_properties.at(@index)
+	else
+	   @index += 1
+        end
+    }
+    return @property_value.value[1..-1].to_f
+  end  
+end
